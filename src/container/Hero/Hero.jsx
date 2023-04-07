eddc19746862b26a347b3efb72728a1b6e3b5d7a
sebastian-plata <113690137+sebastian-plata@users.noreply.github.com>
2023-04-07 16:59:38 +0200

    feat:  create basic jsx for hero section

--- src/container/Hero/Hero.jsx
+++ src/container/Hero/Hero.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import images from '../../constants/images'
 import './Hero.css'
 
-const Hero = () => {
-  return (
-    <div>
-      Hero
-    </div>
+const Hero = () =>  (
+    <section className='app__hero section__padding'>
+      <div className='app__hero-img'>
+        <img src={images.hero} alt="hello world" />
+      </div>
+      <div className='app__hero-info'>
+        <h1 className='app__hero-info_head'></h1>
+        <p className='app__hero-info_para'></p>
+        <a className='app__hero-info_link' href=""></a>
+      </div>
+    </section>
   )
-}
+
 
 export default Hero
