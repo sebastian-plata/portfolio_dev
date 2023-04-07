8d296f9706d5053bd47a6bf55bd6e8e3e8965707
sebastian-plata <113690137+sebastian-plata@users.noreply.github.com>
2023-04-07 17:58:36 +0200

    feat: finish Hero section
    Considering animating the menu icon

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,8 +1,10 @@
 
 import imjuan from "../assets/imjuan.svg";
 import hero from "../assets/hero.png";
+import heroArt from '../assets/heroArt.svg'
 
 export default {
   imjuan,
   hero,
+  heroArt,
 };
--- src/container/Hero/Hero.jsx
+++ src/container/Hero/Hero.jsx
@@ -3,14 +3,14 @@ import images from '../../constants/images'
 import './Hero.css'
 
 const Hero = () =>  (
-    <section className='app__hero section__padding'>
+    <section className='app__hero'>
       <div className='app__hero-img'>
-        <img src={images.hero} alt="hello world" />
+        <img src={images.heroArt} alt="hello world" />
       </div>
       <div className='app__hero-info'>
-        <h1 className='app__hero-info_head'></h1>
-        <p className='app__hero-info_para'></p>
-        <a className='app__hero-info_link' href=""></a>
+        <h1 className='app__hero-info_head'>i&apos;m juan,</h1>
+        <p className='app__hero-info_para'>My goal is to create stuff that people find appealing but also easy to use. </p>
+        <a className='app__hero-info_link' href="">hire me!</a>
       </div>
     </section>
   )
