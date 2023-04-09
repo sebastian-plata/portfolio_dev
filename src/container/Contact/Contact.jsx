7b4bbba51a8cb1035844f26a5d85f1b80992064f
sebastian-plata <113690137+sebastian-plata@users.noreply.github.com>
2023-04-09 14:30:57 +0200

    feat: finish contact section

--- src/container/Contact/Contact.jsx
+++ src/container/Contact/Contact.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Subtitle } from '../../components'
 import './Contact.css'
 
 const Contact = () => {
   return (
-    <div>
-      Contact
+    <div className='app__contact' style={{ backgroundColor: "#f1f1e8" }}>
+      <Subtitle title="come say hi"/>
+      <div className='app__contact-container'>
+        <div className='app__contact-container_email'>
+          <p className='app__contact-container_email-title'>email</p>
+          <p className='app__contact-container_email-para'>sebastian.platap@gmail.com</p>
+        </div>
+        <div className='app__contact-container-phone'>
+          <p className='app__contact-container_phone-title'>phone</p>
+          <p className='app__contact-container_phone-para'>+34 744 66 811</p>
+        </div>
+      </div>
     </div>
   )
 }
